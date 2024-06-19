fe5f5c853f673294eae7f3bb2d2d46743ba25f8d
brillyK <rkdud9798@naver.com>
2024-06-19 12:41:53 +0900

    animation

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,4 +1,3 @@
-// JavaScript 코드
 window.addEventListener('scroll', function () {
     // 현재 스크롤 위치 가져오기
     var scrollY = window.scrollY
@@ -9,14 +8,14 @@ window.addEventListener('scroll', function () {
 
     // k3.png 이미지 초기 위치 설정
     var kImage = document.querySelector('.c-hero__letter-k')
-    var initialPositionX_K = 0 // 초기 위치를 0%로 설정
+    var initialPositionX_K = 0
 
     // b3.png 이미지 왼쪽으로 이동
-    var moveX_B = initialPositionX_B - scrollY / 5 // 움직일 거리 계산
+    var moveX_B = initialPositionX_B - scrollY / 5
     bImage.style.transform = 'translateX(' + moveX_B + '%) translateY(-50%)'
 
     // k3.png 이미지 오른쪽으로 이동
-    var moveX_K = initialPositionX_K + scrollY / 5 // 움직일 거리 계산
+    var moveX_K = initialPositionX_K + scrollY / 5
     kImage.style.transform = 'translateX(' + moveX_K + '%) translateY(-50%)'
 })
 
